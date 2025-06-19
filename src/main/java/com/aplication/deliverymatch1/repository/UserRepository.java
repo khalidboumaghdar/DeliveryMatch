a713cfe45db37b2840ac767ea0883d02f6502aa4
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-06-19 10:09:50 +0100

    j'ai créer User Repository

--- src/main/java/com/aplication/deliverymatch1/repository/UserRepository.java
+++ src/main/java/com/aplication/deliverymatch1/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.aplication.deliverymatch1.repository;
 
-public class UserRepository {
+import com.aplication.deliverymatch1.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
+

163095455abe1e66047aa688ef755b364828b413
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-06-19 10:12:45 +0100

    j'ai créer architecture de project

--- src/test/java/com/aplication/deliverymatch1/DeliveryMatch1ApplicationTests.java
+++ src/test/java/com/aplication/deliverymatch1/DeliveryMatch1ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DeliveryMatch1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

57c9e9d67ba51e48d057d47ab52bae8b1f4932b2
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-06-19 10:03:09 +0100

    j'ai créer config et filter pour authontification

--- src/main/java/com/aplication/deliverymatch1/config/Filter.java
+++ src/main/java/com/aplication/deliverymatch1/config/Filter.java
@@ -1,4 +1,48 @@
 package com.aplication.deliverymatch1.config;
 
-public class Filter {
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.Collection;
+
+public class Filter implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        String redirectUrl = "/home";
+
+        for (GrantedAuthority authority : authorities) {
+            String role = authority.getAuthority();
+
+            if (role.equals("ROLE_ADMIN")) {
+                redirectUrl = "/admin/dashboard";
+                break;
+            } else if (role.equals("ROLE_Conducteur")) {
+                redirectUrl = "/conducteur/dashboard";
+                break;
+            } else if (role.equals("ROLE_Expediteur")) {
+                redirectUrl = "/expediteur/dashboard";
+                break;
+            } else if (role.equals("ROLE_USER")) {
+                redirectUrl = "/user/dashboard";
+                break;
+            }
+        }
+
+        response.sendRedirect(redirectUrl);
+    }
 }
+

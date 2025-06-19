f65d86c193c93bd5de34609edb6040a7f80697d8
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-06-19 10:06:07 +0100

    j'ai créer DTO

--- src/main/java/com/aplication/deliverymatch1/dto/AuthResponse.java
+++ src/main/java/com/aplication/deliverymatch1/dto/AuthResponse.java
@@ -1,4 +1,21 @@
 package com.aplication.deliverymatch1.dto;
 
+import com.aplication.deliverymatch1.entity.User;
+
 public class AuthResponse {
+    private String token;
+    private User user;
+
+    public AuthResponse(String token, User user) {
+        this.token = token;
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }

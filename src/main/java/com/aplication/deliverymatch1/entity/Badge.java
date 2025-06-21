eca658463816904f98c79a222e0c2f4cd4b5e1b5
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-06-21 18:09:11 +0100

    j'ai créer antity Badge

--- src/main/java/com/aplication/deliverymatch1/entity/Badge.java
+++ src/main/java/com/aplication/deliverymatch1/entity/Badge.java
@@ -1,4 +1,53 @@
 package com.aplication.deliverymatch1.entity;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Badge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String label = "Vérifié";
+
+    private LocalDate dateAttribution = LocalDate.now();
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public LocalDate getDateAttribution() {
+        return dateAttribution;
+    }
+
+    public void setDateAttribution(LocalDate dateAttribution) {
+        this.dateAttribution = dateAttribution;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }

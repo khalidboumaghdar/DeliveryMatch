73195661acb4b033e55619b57c5548a1b1a4042a
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-06-21 18:13:28 +0100

    j'ai créer AnnonceRepository

--- src/main/java/com/aplication/deliverymatch1/repository/AnnonceRepository.java
+++ src/main/java/com/aplication/deliverymatch1/repository/AnnonceRepository.java
@@ -1,4 +1,10 @@
 package com.aplication.deliverymatch1.repository;
 
-public class AnnonceRepository {
+import com.aplication.deliverymatch1.entity.Annonce;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnnonceRepository extends JpaRepository<Annonce, Long> {
+
 }

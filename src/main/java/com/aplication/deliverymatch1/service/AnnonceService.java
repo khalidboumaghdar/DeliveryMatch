42eab2f2335e8946b3b400c4633ffee85dc4a0d2
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-06-21 18:17:58 +0100

    j'ai créer AnnonceService

--- src/main/java/com/aplication/deliverymatch1/service/AnnonceService.java
+++ src/main/java/com/aplication/deliverymatch1/service/AnnonceService.java
@@ -1,4 +1,33 @@
 package com.aplication.deliverymatch1.service;
 
+import com.aplication.deliverymatch1.entity.Annonce;
+import com.aplication.deliverymatch1.repository.AnnonceRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AnnonceService {
+
+    private final AnnonceRepository annonceRepository;
+
+    public AnnonceService(AnnonceRepository annonceRepository) {
+        this.annonceRepository = annonceRepository;
+    }
+
+    public Annonce createAnnonce(Annonce annonce) {
+        return annonceRepository.save(annonce);
+    }
+
+    public List<Annonce> getAllAnnonces() {
+        return annonceRepository.findAll();
+    }
+
+    public Annonce getAnnonceById(Long id) {
+        return annonceRepository.findById(id).orElseThrow();
+    }
+
+    public void deleteAnnonce(Long id) {
+        annonceRepository.deleteById(id);
+    }
 }

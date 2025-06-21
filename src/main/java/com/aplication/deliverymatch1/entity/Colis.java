9e7b723e7a52095b42e5534039e2d7269cc95879
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2025-06-21 18:09:56 +0100

    j'ai créer antity Colis

--- src/main/java/com/aplication/deliverymatch1/entity/Colis.java
+++ src/main/java/com/aplication/deliverymatch1/entity/Colis.java
@@ -1,4 +1,48 @@
 package com.aplication.deliverymatch1.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Colis {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+    private double poids;
+    private String dimensions;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPoids() {
+        return poids;
+    }
+
+    public void setPoids(double poids) {
+        this.poids = poids;
+    }
+
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+    }
 }
